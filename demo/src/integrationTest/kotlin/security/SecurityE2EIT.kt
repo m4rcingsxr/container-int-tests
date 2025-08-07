7337221b9eeb66565899a0235d4e274261f80b4d
Marcin Seweryn <marcinsewerynn@gmail.com>
2025-08-07 15:40:54 +0200

    compose success

--- demo/src/integrationTest/kotlin/security/SecurityE2EIT.kt
+++ demo/src/integrationTest/kotlin/security/SecurityE2EIT.kt
@@ -2,10 +2,14 @@ package security
 
 import io.quarkus.test.junit.QuarkusIntegrationTest
 import org.junit.jupiter.api.Test
+import testrepositories.SessionInitializer
 
 
 @QuarkusIntegrationTest
 class SecurityE2EIT {
-    @Test
-    fun `sessionFactory initialized sucessfully`() {}
+  @Test
+  fun `sessionFactory initialized sucessfully`() {
+    val sessionFactory = SessionInitializer.sessionFactory
+    println("FACTORY OPEN " + sessionFactory.isOpen)
+  }
 }
--- demo/src/integrationTest/kotlin/testrepositories/SessionInitializer.kt
+++ demo/src/integrationTest/kotlin/testrepositories/SessionInitializer.kt
@@ -6,15 +6,13 @@ import org.hibernate.cfg.Configuration
 
 internal object SessionInitializer {
 
-  private const val PORT = "quarkus.datasource.devservices.port"
-
   val sessionFactory = initializeSessionFactory()
 
   private fun initializeSessionFactory(): SessionFactory {
     return Configuration().apply {
       setProperty(AvailableSettings.JAKARTA_JDBC_URL, "jdbc:postgresql://localhost:55432/quarkus")
       setProperty(AvailableSettings.JAKARTA_JDBC_USER, "quarkus")
-      setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "password")
+      setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "quarkus")
       setProperty(AvailableSettings.SHOW_SQL, true)
       setProperty(AvailableSettings.FORMAT_SQL, true)
     }

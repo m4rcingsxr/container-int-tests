d6fb5472a51f57152f80aa2079f67e788837b1a1
Marcin Seweryn <marcinsewerynn@gmail.com>
2025-08-07 16:51:48 +0200

    3.25.1

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  kotlin("jvm") version "2.1.21"
-  kotlin("plugin.allopen") version "2.1.21"
-  kotlin("plugin.serialization") version "2.1.21"
+  kotlin("jvm") version "2.2.0"
+  kotlin("plugin.allopen") version "2.2.0"
+  kotlin("plugin.serialization") version "2.2.0"
   kotlin("plugin.jpa") version "2.2.0"
   id("io.quarkus")
 }
